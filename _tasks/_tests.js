25959ac04267a8ee90668d3cf973312f96c06a55
Erwin GO <erwingaitano@gmail.com>
2017-06-19 20:23:08 -0500

    adding tests for the update tasks

---
+++ _tasks/_tests.js
@@ -0,0 +1,129 @@
+const { getChildDirs, getChildFiles } = require('/Users/erwin/Projects/node-helpers/file');
+const path = require('path');
+const assert = require('assert');
+
+const mediaPath = '/Users/erwin/MEGAsync/music-player-files/_media';
+
+// all dirs
+(function init() {
+  const hasBadDirs = getChildDirs(mediaPath, { recursive: true }).some(el => el.includes('~'));
+  assert.equal(hasBadDirs, false, 'Dirs must not contain ~ in their name');
+}());
+
+// _artists dirs
+(function init() {
+  const artists = getChildDirs(path.join(mediaPath, '_artists')).map(el => el.split('/').pop());
+  const files = getChildFiles(path.join(mediaPath, '_artists'));
+
+  assert.equal(artists.some(el => el[0] === '_'), false, 'it should not have underscore dirs');
+  assert(artists.length >= 1, 'it should have one or more dirs');
+  assert.equal(files.length, 0, 'it should not have files');
+}());
+
+// artists dirs
+(function init() {
+  const artistsDirs = getChildDirs(path.join(mediaPath, '_artists'));
+  artistsDirs.forEach(el => {
+    const dirs = getChildDirs(el);
+    const files = getChildFiles(el);
+
+    const hasBadDirs = dirs.some(el => {
+      const dirName = el.split('/').pop();
+      if (dirName[0] !== '_') return false;
+      if (dirName === '_covers' || dirName === '_albums') return false;
+      return true;
+    });
+
+    const hasGoodFiles = files.length === 0 ? true : (files.length === 1 && files[0].split('/').pop() === '.DS_Store');
+
+    assert(dirs.length >= 1, 'should have dirs');
+    assert(!hasBadDirs, 'can only contain song, _covers/_albums dirs');
+    assert(hasGoodFiles, 'should not have files or at least only have .DS_Store');
+  });
+}());
+
+// _albums dirs
+(function init() {
+  const albumsDirs = getChildDirs(path.join(mediaPath, '_artists'), { recursive: true })
+    .filter(el => el.split('/').pop() === '_albums');
+
+  albumsDirs.forEach(el => {
+    const dirs = getChildDirs(el);
+    const files = getChildFiles(el);
+    const hasBadDirs = dirs.some(el => el.split('/').pop()[0] === '_');
+    const hasGoodFiles = files.length === 0 ? true : (files.length === 1 && files[0].split('/').pop() === '.DS_Store');
+
+    assert(!hasBadDirs, 'can only contain album dirs');
+    assert(hasGoodFiles, 'should not have files or at least only have .DS_Store');
+  });
+}());
+
+// album dirs
+(function init() {
+  const albumsDirs = getChildDirs(path.join(mediaPath, '_artists'), { recursive: true })
+    .filter(el => el.split('/').pop() === '_albums');
+
+  albumsDirs.forEach(el => {
+    const dirs = getChildDirs(el);
+
+    dirs.forEach(el => {
+      const dirs = getChildDirs(el);
+      const files = getChildFiles(el);
+      const hasBadDirs = dirs.some(el => {
+        const dirName = el.split('/').pop();
+        if (dirName[0] !== '_') return false;
+        if (dirName === '_covers') return false;
+        return true;
+      });
+      const hasGoodFiles = files.length === 0 ? true : (files.length === 1 && files[0].split('/').pop() === '.DS_Store');
+
+      assert(!hasBadDirs, 'can only contain _covers dir and song dirs');
+      assert(hasGoodFiles, 'should not have files or at least only have .DS_Store');
+    });
+  });
+}());
+
+// song dirs
+(function init() {
+  const songDirs = getChildDirs(mediaPath, { recursive: true })
+    .filter(el => {
+      const dirEls = el.split('/');
+      const dirName = dirEls[dirEls.length - 1];
+      const dirParentName = dirEls[dirEls.length - 2];
+
+      if (dirName[0] === '_') return false;
+      if (dirParentName === '_albums' || dirParentName === '_artists') return false;
+      return true;
+    });
+
+  songDirs.forEach(el => {
+    const dirs = getChildDirs(el);
+    const files = getChildFiles(el);
+
+    const hasBadDirs = dirs.some(el => el.split('/').pop() !== '_covers');
+    const hasBadFiles = files.some(el => {
+      const name = el.split('/').pop();
+      if (name === '_file.mp3' || name === 'revisedBestAudio' ||
+        name === '_file.m4a' || name === '_lyrics.txt' || name === '.DS_Store') return false;
+      return true;
+    });
+
+    assert(!hasBadFiles, 'must contain 1 _file.[mp3|m4a] file, can contain 1 _lyrics.txt file, can contain .DS_Store file');
+    assert(!hasBadDirs, 'can only contain _covers dir');
+  });
+}());
+
+// cover folders
+(function init() {
+  const coverDirs = getChildDirs(mediaPath, { recursive: true })
+    .filter(el => el.split('/').pop() === '_covers');
+
+  coverDirs.forEach(el => {
+    const dirs = getChildDirs(el);
+    const files = getChildFiles(el).map(el => el.split('/').pop()).filter(el => el !== '.DS_Store');
+
+    const hasBadFiles = files.some(el => !(/\.jpg$/.test(el)));
+    assert(dirs.length === 0, 'must not contain folders');
+    assert(!hasBadFiles, 'must have 1 or more jpg files and can contain .DS_Store');
+  });
+}());
--- todo.md
+++ todo.md
@@ -1,3 +1,62 @@
+```bash
+$ folderStructureChecker .
+```
+
+- all custom code should be inside the most inner component folder available
+- when we have shared code, we send it up to the most common inner component between those who used
+  the shared code
+
+==================
+
+  - plan to follow
+  - only do the mobile part
+  - get the 2 months payed
+
+- Front-End web developer
+https://www.toptal.com/front-end/job-description
+
+Description:
+Front-End Web Developer who is motivated to combine the art of design with the art of programming. Responsibilities will include translation of the UI/UX design wireframes to actual code that will produce visual elements of the application. You will work with the UI/UX designer and bridge the gap between graphical design and technical implementation, taking an active role on both sides and defining how the application looks as well as how it works.
+
+Skills:
+  - translation of the UI/UX design wireframes (Sketch/Photoshop) to actual code that will produce visual elements of the application.
+  - Proficient understanding of web markup, including HTML5, CSS3
+  - CSS pre-processing platforms, such as LESS and SASS
+  - Good understanding of responsive web design
+  - Proficient understanding of Javascript
+  - Good understanding and experience with ReactJS, asynchronous request handling, partial page updates, and AJAX
+  - Good understanding of client side building tools like Webpack
+  - Proficient understanding of cross-browser compatibility issues and ways to work around them.
+  - Understanding of SEO principles and ensuring that application will adhere to them
+  - Knowledge about versioning tools like Git
+  - Basic understanding of server-side (NodeJS)
+  - Computer Science eng. or similar background is a plus
+
+- ios developer
+https://www.toptal.com/ios/job-description
+
+Description:
+iOS developer responsible for the development and maintenance of applications aimed at a range of iOS mobile devices. Your primary focus will be development of iOS applications and their integration with back-end services. You will be working alongside other engineers and developers working on different layers of the infrastructure. Therefore, a commitment to collaborative problem solving, sophisticated design, and the creation of quality products is essential.
+
+Responsibilities:
+  - Design and build applications for the iOS platform
+  - Ensure the performance, quality, and responsiveness of applications
+  - Collaborate with a team to define, design, and ship new features
+  - Identify and correct bottlenecks and fix bugs
+  - Help maintain code quality, organization, and automatization
+
+Skills:
+  - Proficient with the programming language Swift, and Cocoa Touch (Objective-C is a plus)
+  - Experience with iOS frameworks such as Core Data, Core Animation, etc.
+  - Experience with offline storage, threading, and performance tuning
+  - Familiarity with RESTful APIs to connect iOS applications to back-end services
+  - Knowledge of other web technologies and UI/UX standards
+  - Understanding of Apple’s design principles and interface guidelines
+  - Familiarity with cloud message APIs and push notifications
+  - Proficient understanding of code versioning tools such as Git
+  - Familiarity with continuous integration
+  - Computer Science eng. or similar background is a plus
+
 
 # TODO
 - verify that the folders are in a consistent state (this probably should be a test)
