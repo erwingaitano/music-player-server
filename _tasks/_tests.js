b07d851b15435be458c2ddda10b56044a1c83c7e
Erwin GO <erwingaitano@gmail.com>
2017-06-24 00:43:54 -0500

    updating node-helpers version

--- _tasks/_tests.js
+++ _tasks/_tests.js
@@ -1,18 +1,19 @@
-const { getChildDirs, getChildFiles } = require('/Users/erwin/Projects/node-helpers/file');
+const nodeHelpers = require('node-helpers');
 const path = require('path');
 const assert = require('assert');
 
+const { getChildFolders, getChildFiles } = nodeHelpers.file;
 const mediaPath = '/Users/erwin/MEGAsync/music-player-files/_media';
 
 // all dirs
 (function init() {
-  const hasBadDirs = getChildDirs(mediaPath, { recursive: true }).some(el => el.includes('~'));
+  const hasBadDirs = getChildFolders(mediaPath, { recursive: true }).some(el => el.includes('~'));
   assert.equal(hasBadDirs, false, 'Dirs must not contain ~ in their name');
 }());
 
 // _artists dirs
 (function init() {
-  const artists = getChildDirs(path.join(mediaPath, '_artists')).map(el => el.split('/').pop());
+  const artists = getChildFolders(path.join(mediaPath, '_artists')).map(el => el.split('/').pop());
   const files = getChildFiles(path.join(mediaPath, '_artists'));
 
   assert.equal(artists.some(el => el[0] === '_'), false, 'it should not have underscore dirs');
@@ -22,9 +23,9 @@ const mediaPath = '/Users/erwin/MEGAsync/music-player-files/_media';
 
 // artists dirs
 (function init() {
-  const artistsDirs = getChildDirs(path.join(mediaPath, '_artists'));
+  const artistsDirs = getChildFolders(path.join(mediaPath, '_artists'));
   artistsDirs.forEach(el => {
-    const dirs = getChildDirs(el);
+    const dirs = getChildFolders(el);
     const files = getChildFiles(el);
 
     const hasBadDirs = dirs.some(el => {
@@ -34,40 +35,37 @@ const mediaPath = '/Users/erwin/MEGAsync/music-player-files/_media';
       return true;
     });
 
-    const hasGoodFiles = files.length === 0 ? true : (files.length === 1 && files[0].split('/').pop() === '.DS_Store');
-
     assert(dirs.length >= 1, 'should have dirs');
     assert(!hasBadDirs, 'can only contain song, _covers/_albums dirs');
-    assert(hasGoodFiles, 'should not have files or at least only have .DS_Store');
+    assert.equal(files.length, 0, 'should not have files');
   });
 }());
 
 // _albums dirs
 (function init() {
-  const albumsDirs = getChildDirs(path.join(mediaPath, '_artists'), { recursive: true })
+  const albumsDirs = getChildFolders(path.join(mediaPath, '_artists'), { recursive: true })
     .filter(el => el.split('/').pop() === '_albums');
 
   albumsDirs.forEach(el => {
-    const dirs = getChildDirs(el);
+    const dirs = getChildFolders(el);
     const files = getChildFiles(el);
     const hasBadDirs = dirs.some(el => el.split('/').pop()[0] === '_');
-    const hasGoodFiles = files.length === 0 ? true : (files.length === 1 && files[0].split('/').pop() === '.DS_Store');
 
     assert(!hasBadDirs, 'can only contain album dirs');
-    assert(hasGoodFiles, 'should not have files or at least only have .DS_Store');
+    assert.equal(files.length, 0, 'should not have files');
   });
 }());
 
 // album dirs
 (function init() {
-  const albumsDirs = getChildDirs(path.join(mediaPath, '_artists'), { recursive: true })
+  const albumsDirs = getChildFolders(path.join(mediaPath, '_artists'), { recursive: true })
     .filter(el => el.split('/').pop() === '_albums');
 
   albumsDirs.forEach(el => {
-    const dirs = getChildDirs(el);
+    const dirs = getChildFolders(el);
 
     dirs.forEach(el => {
-      const dirs = getChildDirs(el);
+      const dirs = getChildFolders(el);
       const files = getChildFiles(el);
       const hasBadDirs = dirs.some(el => {
         const dirName = el.split('/').pop();
@@ -75,17 +73,16 @@ const mediaPath = '/Users/erwin/MEGAsync/music-player-files/_media';
         if (dirName === '_covers') return false;
         return true;
       });
-      const hasGoodFiles = files.length === 0 ? true : (files.length === 1 && files[0].split('/').pop() === '.DS_Store');
 
       assert(!hasBadDirs, 'can only contain _covers dir and song dirs');
-      assert(hasGoodFiles, 'should not have files or at least only have .DS_Store');
+      assert.equal(files.length, 0, 'should not have files');
     });
   });
 }());
 
 // song dirs
 (function init() {
-  const songDirs = getChildDirs(mediaPath, { recursive: true })
+  const songDirs = getChildFolders(mediaPath, { recursive: true })
     .filter(el => {
       const dirEls = el.split('/');
       const dirName = dirEls[dirEls.length - 1];
@@ -97,7 +94,7 @@ const mediaPath = '/Users/erwin/MEGAsync/music-player-files/_media';
     });
 
   songDirs.forEach(el => {
-    const dirs = getChildDirs(el);
+    const dirs = getChildFolders(el);
     const files = getChildFiles(el).map(el => el.split('/').pop());
 
     const mediaFilesCount = files
@@ -110,31 +107,27 @@ const mediaPath = '/Users/erwin/MEGAsync/music-player-files/_media';
         return false;
       }
 
-      if (name === 'revisedBestAudio' ||
-          name === '_lyrics.txt' ||
-          name === '.DS_Store') {
-        return false;
-      }
+      if (name === 'revisedBestAudio' || name === '_lyrics.txt') return false;
       return true;
     });
 
     assert.equal(mediaFilesCount, 1, 'must contain 1 _file.[mp3|m4a]');
-    assert(!hasBadFiles, 'can contain 1 _lyrics.txt file, can contain .DS_Store file');
+    assert(!hasBadFiles, 'can contain 1 _lyrics.txt file');
     assert(!hasBadDirs, 'can only contain _covers dir');
   });
 }());
 
 // cover dirs
 (function init() {
-  const coverDirs = getChildDirs(mediaPath, { recursive: true })
+  const coverDirs = getChildFolders(mediaPath, { recursive: true })
     .filter(el => el.split('/').pop() === '_covers');
 
   coverDirs.forEach(el => {
-    const dirs = getChildDirs(el);
-    const files = getChildFiles(el).map(el => el.split('/').pop()).filter(el => el !== '.DS_Store');
+    const dirs = getChildFolders(el);
+    const files = getChildFiles(el).map(el => el.split('/').pop());
 
     const hasBadFiles = files.some(el => !(/\.jpg$/.test(el)));
     assert(dirs.length === 0, 'must not contain dirs');
-    assert(!hasBadFiles, 'must have 1 or more jpg files and can contain .DS_Store');
+    assert(!hasBadFiles, 'must have 1 or more jpg files');
   });
 }());
